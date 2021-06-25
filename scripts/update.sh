7756f06917d38956ef8b2c89a9d1c5ca437f1f18
James Taylor <jamest@uk.ibm.com>
2021-06-25 12:21:11 +0100

    Include repo name in data
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,6 +8,6 @@
 for repo in fabric fabric-ca fabric-chaincode-java fabric-chaincode-node fabric-contract-api-go fabric-gateway-java fabric-sdk-go fabric-sdk-java fabric-sdk-node
 do
   echo "Repo: $repo"
-  curl --silent "https://api.github.com/repos/hyperledger/${repo}/releases/latest" | jq '{ version: .tag_name, published: .published_at, url: .html_url, days_since: (((now - ( .published_at | fromdateiso8601? )) / 86400) | floor ) }' > ${GITHUB_WORKSPACE}/docs/_data/repos/${repo}.json
+  curl --silent "https://api.github.com/repos/hyperledger/${repo}/releases/latest" | jq --arg repo "${repo}" '{ name: $repo, version: .tag_name, published: .published_at, url: .html_url, days_since: (((now - ( .published_at | fromdateiso8601? )) / 86400) | floor ) }' > ${GITHUB_WORKSPACE}/docs/_data/repos/${repo}.json
   cat ${GITHUB_WORKSPACE}/docs/_data/repos/${repo}.json
 done
