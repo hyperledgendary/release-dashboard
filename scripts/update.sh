5522d44833fc46016e8c1d5df9726d040162d61c
James Taylor <jt-nti@users.noreply.github.com>
2021-11-25 14:57:06 +0000

    Add fabric-gateway repository

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,7 +7,7 @@
 : "${RUNNER_TEMP:?}"
 
 mkdir -p "${RUNNER_TEMP:?}/releases"
-for repo in fabric fabric-ca fabric-chaincode-java fabric-chaincode-node fabric-contract-api-go fabric-gateway-java fabric-sdk-go fabric-sdk-java fabric-sdk-node
+for repo in fabric fabric-ca fabric-chaincode-java fabric-chaincode-node fabric-contract-api-go fabric-gateway-java fabric-gateway fabric-sdk-go fabric-sdk-java fabric-sdk-node
 do
   echo "Repo: $repo"
   curl --silent "https://api.github.com/repos/hyperledger/${repo}/releases/latest" | jq --arg repo "${repo}" '{ name: $repo, version: .tag_name, published: .published_at, url: .html_url, days_since: (((now - ( .published_at | fromdateiso8601? )) / 86400) | floor ) }' > "${RUNNER_TEMP:?}/releases/${repo}.json"
